3fefb3dc420d912f8cf75ef56d7dacd7df98cb00
Ultifeek <ultifeek1@gmail.com>
2023-05-14 23:40:13 +0000

    function that retrieves an element from a list like in C

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/urs/bin/python3
+def element_at(my_list, idx):
+    length = len(my_list) - 1
+
+    if idx < 0:
+        return (None)
+
+    if idx > length:
+        return (None)
+
+    return(my_list[idx])
