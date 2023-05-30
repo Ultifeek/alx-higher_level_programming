f8bffd111727a439a26d10f4eb9b48f3e05f8722
Ultifeek <ultifeek1@gmail.com>
2023-05-30 23:09:26 +0000

    Write an empty class Square that defines a square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,4 +2,10 @@
 class Square:
     """Class Square that defines a square.
     """
-    pass
+    def __init__(self, size):
+        """initializing class Square
+        Args:
+        size: size of square
+
+        """
+        self.__size = size
